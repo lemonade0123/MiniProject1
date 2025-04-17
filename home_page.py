0f7b43ef7961465043787f2a7949142296409e90
이도건 <rookies.dogun@gmail.com>
2025-04-17 17:49:42 +0900

    sideBar Navigation 제거

--- home_page.py
+++ home_page.py
@@ -1,10 +1,6 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
 
-page = st.sidebar.radio("페이지 선택", ["홈", "페이지1", "페이지2"])
-
-if page == "홈":
-    st.write("🏠 홈입니다.")
-elif page == "페이지1":
-    st.write("📄 페이지1입니다.")
-elif page == "페이지2":
-    st.write("📄 페이지2입니다.")
+load_dotenv()
+st.set_page_config(initial_sidebar_state="collapsed")
