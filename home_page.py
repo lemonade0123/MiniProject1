e0ab1e449b3f1a5249c7f87db5dc782d4f763fa2
lemonade0123 <gustmd6895@hanmail.net>
2025-04-18 09:36:44 +0900

    1

--- home_page.py
+++ home_page.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-page = st.sidebar.radio("페이지 선택", ["홈", "페이지1", "페이지2"])
+page = st.sidebar.radio("")
 
 if page == "홈":
     st.write("🏠 홈입니다.")
@@ -8,3 +8,8 @@ elif page == "페이지1":
     st.write("📄 페이지1입니다.")
 elif page == "페이지2":
     st.write("📄 페이지2입니다.")
+    
+#사이드바에 검색 옵션 구성
+st.sidebar.header("검색 옵션")
+search_query = st.sidebar.text_input("검색어", "파이썬")
+display_count = st.sidebar.slider("검색 결과 수", 10, 100, 50)
