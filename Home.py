ff4de844d569c5610732d56368b5c0ad616a3ae2
이도건 <rookies.dogun@gmail.com>
2025-04-18 09:29:51 +0900

    sidebar 제작

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 from dotenv import load_dotenv
-
+from components.sidebar import render_sidebar
 load_dotenv()
-st.set_page_config(initial_sidebar_state="collapsed")
+
+render_sidebar()
